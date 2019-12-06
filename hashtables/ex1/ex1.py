2535e3145cf722fe50057b838d807d4be0465cc6
Clark Williams <theclarkwilliams@gmail.com>
2019-12-06 14:28:30 -0700

    ex1 tests passing, with loop(s)

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,7 +13,42 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
-    return None
+    answer = []
+
+    if length <= 1:
+        return None
+    for k, v in enumerate(weights):
+        # print(f"k, v: {v, k}")
+        hash_table_insert(ht, v, k)
+        # print(hash_table_retrieve(ht, v))
+        # if hash_table_retrieve(limit - weight):
+        # print(hash_table_retrieve(ht, limit - k))
+    for w in weights:
+        print(hash_table_retrieve(ht, limit - w))
+        result = hash_table_retrieve(ht, limit - w)
+        # print("result {result} with w {w}")
+        if result is not None:
+            answer.append(result)
+            # answer.remove(None)
+        # else:
+        #     answer[0] = None
+        # answer.append(result)
+        print("answer", answer)
+        # if answer[0] == answer[1]:
+        #     answer[0] = 0
+        # if len(answer) == 1:
+        #     answer = [0, answer[0]]
+        #     answer.pop()
+        answer.sort(reverse=True)
+    if answer[0] == answer[1]:
+        answer[1] = 0
+        
+
+    # print("answer", answer)
+    return tuple(answer)
+    # tuple(answer)
+    # return print_answer(answer)
+    # return None
 
 
 def print_answer(answer):
@@ -21,3 +56,5 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+get_indices_of_item_weights([4, 4], 2, 8)
